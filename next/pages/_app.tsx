c75813b3c50bc3793a87ca166fbb557c4d001d04
umihico <umihico@users.noreply.github.com>
2022-03-04 23:32:51 +0900

    feat: insert ChakraProvider

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,10 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import "../styles/index.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
