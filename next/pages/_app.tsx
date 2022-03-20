6521d4c8b331e2f9196587d6af93c22e9ff8778b
umihico <umihico@users.noreply.github.com>
2022-03-21 08:24:04 +0900

    cmd: npx prettier --write "**/*.{json,js,ts,tsx}"

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,10 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { AppProps } from "next/app";
-import "../styles/index.css";
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import '../styles/index.css'
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
