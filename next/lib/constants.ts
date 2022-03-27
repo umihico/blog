4317261f8bae3f5a8968b65ccffafe4ed8981df6
umihico <umihico@users.noreply.github.com>
2022-03-27 09:00:50 +0900

    feat: modify blog title

--- next/lib/constants.ts
+++ next/lib/constants.ts
@@ -1,4 +1,5 @@
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL
+export const BLOG_TITLE = process.env.NEXT_PUBLIC_BLOG_TITLE
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
     'https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
