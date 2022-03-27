5c51605ab9edede99745c1cc869b4d6ccd8ec62a
umihico <umihico@users.noreply.github.com>
2022-03-27 22:55:42 +0900

    feat: use marked instead of remark

--- next/lib/markdownToHtml.ts
+++ next/lib/markdownToHtml.ts
@@ -1,7 +1,6 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { marked } from 'marked'
 
 export default async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).process(markdown)
+    const result = await marked.parse(markdown)
     return result.toString()
 }
