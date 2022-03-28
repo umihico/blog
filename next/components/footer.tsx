c9a7e52b389438dece4c336b254ff95c367c95e5
umihico <umihico@users.noreply.github.com>
2022-03-28 09:22:37 +0900

    feat: modify footer

--- next/components/footer.tsx
+++ next/components/footer.tsx
@@ -10,18 +10,16 @@ const Footer = () => {
                         Statically Generated with Next.js.
                     </h3>
                     <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-                        <a
-                            href="https://nextjs.org/docs/basic-features/pages"
-                            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-                        >
-                            Read Documentation
-                        </a>
-                        <a
-                            href={`${GITHUB_URL}`}
-                            className="mx-3 font-bold hover:underline"
-                        >
-                            View on GitHub
-                        </a>
+                        <div className="mx-3 font-bold justify-center text-center">
+                            The source code for this blog is
+                            <a
+                                href={`${GITHUB_URL}`}
+                                className="underline hover:text-blue-600 duration-200 transition-colors"
+                            >
+                                {' '}
+                                available on GitHub{' '}
+                            </a>
+                        </div>
                     </div>
                 </div>
             </Container>
