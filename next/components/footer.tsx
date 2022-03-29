85b882b80a0f26d73dc59c341a20fc28b8c8fa72
umihico <umihico@users.noreply.github.com>
2022-03-30 07:46:05 +0900

    fix: 'Warning: Each child in a list should have a unique "key" prop'

--- next/components/footer.tsx
+++ next/components/footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
                 <div className="py-28 flex flex-col lg:flex-row items-center">
                     <div className="tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
                         {Object.entries(socials).map(([url, icon], index) => (
-                            <a href={url}>
+                            <a href={url} key={index}>
                                 <FontAwesomeIcon
                                     className={`inline-block h-10 ${
                                         index + 1 < Object.keys(socials).length
