42ed1ebac9d354be51153f1f28030fb6be16f6d1
umihico <umihico@users.noreply.github.com>
2022-12-17 23:21:10 +0900

    feat: add Google Tag Manager

--- next/lib/constants.ts
+++ next/lib/constants.ts
@@ -1,4 +1,5 @@
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL
 export const BLOG_TITLE = process.env.NEXT_PUBLIC_BLOG_TITLE
 export const BLOG_DESCRIPTION = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION
+export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 export const HOME_OG_IMAGE_URL = `https://${process.env.NEXT_PUBLIC_HOST_NAME}/favicon/mstile-310x310.png`
