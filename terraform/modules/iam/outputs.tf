34bcd870fa830f19beedb71055ddd57b34e99eb5
umihico <umihico@users.noreply.github.com>
2023-06-13 09:07:30 +0900

    Refactor IAM module and data source for GitHub Actions

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,6 +1,6 @@
 output "all" {
   value = {
-    oidc_iam_openid_connect_provider = aws_iam_openid_connect_provider.github_actions
+    oidc_iam_openid_connect_provider = data.aws_iam_openid_connect_provider.github_actions
     oidc_iam_role                    = aws_iam_role.github_actions
   }
 }
