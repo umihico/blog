6aea7a4c8d2f7c59e971c5f49ae3e3571883a229
umihico <umihico@users.noreply.github.com>
2024-04-06 18:56:45 +0700

    feat: logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
+            <div className="hidden">
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
